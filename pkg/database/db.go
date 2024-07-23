a160b0200cf9863fcdc36d08edd881cb2aec08be
pavanmuttange <pavanmuttange@gmail.com>
2024-07-23 14:50:55 +0530

    k8s changes

--- pkg/database/db.go
+++ pkg/database/db.go
@@ -4,16 +4,18 @@ import (
 	"log"
 
 	"github.com/pavanmuttange/go-folder-structure/models"
-	"gorm.io/driver/sqlite"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func InitDB() *gorm.DB {
-	db, err := gorm.Open(sqlite.Open("users.db"), &gorm.Config{})
+	dsn := "host=localhost user=postgres password=postgres dbname=postgres port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal("Failed to connect database: ", err)
+		log.Fatal("Failed to connect to database: ", err)
 	}
 
+	// Auto migrate your models
 	db.AutoMigrate(&models.User{})
 
 	return db
